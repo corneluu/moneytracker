609152318a6f7477c67bbeefa6f4735b6c74a3dd
corneluu <mcornel1178@gmail.com>
2026-04-20 23:15:12 +0300

    fix: correct GitHub Pages base path to /moneytracker/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/money-tracker/', // GitHub Pages repo name
+  base: '/moneytracker/', // GitHub Pages repo name
 });
